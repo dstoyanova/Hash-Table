f1c75e5f8c68a45a51a5b4896338056a1786c9a1
Desislava Stoyanova <dess.stoyanova@mail.bg>
2015-10-17 19:15:10 +0200

    Initial Commit

---
+++ Hash-Table/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Hash-Table
+//
+//  Created by Desislava Stoyanova on 10/17/15.
+//  Copyright © 2015 Desislava Stoyanova. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
